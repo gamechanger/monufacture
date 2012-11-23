f8447d7920fdabaad579981a26b1439458c4e67a
Tom Leach <tom@gc.io>
2012-11-23 16:44:34 -0500

    Implemented basic factory class.

--- monufacture/factory.py
+++ monufacture/factory.py
@@ -1,21 +1,42 @@
 from types import FunctionType
 
-
 class Factory(object):
-    def __init__(self, database, collection, **attrs):
-        self.database = database
+    def __init__(self, collection, **attrs):
         self.collection = collection
         self.attrs = attrs
 
-    def build(self):
-        """Builds an instance of the document described by the sttributes
-        provided to this factory without actually persisting it to 
-        the database."""
+    def build(self, **overrides):
+        """Builds an instance of the document described by the attributes
+        used to create this factory without actually persisting it to 
+        the database. Any overrides provided are used in preference to 
+        those attributes associated with the factory."""
         doc = {}
+
+        # Do two passes, the first to set the static values, the second
+        # to apply functions which may or not depend on static values.
+        for key, value in self.attrs.items():
+            if not isinstance(value, FunctionType):
+                # Use the override if present
+                if key in overrides:
+                    doc[key] = overrides[key]
+                    continue
+
+                doc[key] = value
+
         for key, value in self.attrs.items():
             if isinstance(value, FunctionType):
-                doc[key] = value()
-            else:
-                doc[key] = value
+                # Use the override if present
+                if key in overrides:
+                    doc[key] = overrides[key]
+                    continue
+
+                doc[key] = value(doc)
+
         return doc
 
+    def create(self, **overrides):
+        """Builds an instance of the document using the same approach as 
+        `build` but also persists the document to the database."""
+        doc = self.build(**overrides)
+        doc_id = self.collection.insert(doc)
+        return self.collection.find_one(doc_id)
--- tests/factory_tests.py
+++ tests/factory_tests.py
@@ -1,16 +1,16 @@
 import unittest
 from monufacture.factory import Factory
 from mock import Mock
+from bson.objectid import ObjectId
 
 
 class TestFactory(unittest.TestCase):
 
     def setUp(self):
-        self.database = Mock()
         self.collection = Mock()
 
-    def test_build_simple_factory(self):
-        factory = Factory(self.database, self.collection,
+    def test_build_simple(self):
+        factory = Factory(self.collection,
             first_name='John',
             last_name='Smith',
             age=32)
@@ -21,17 +21,100 @@ class TestFactory(unittest.TestCase):
             "age": 32
         })
 
-    def test_build_factory_with_function_attrs(self):
-        def age_function():
-            return 32
+    def test_build_with_function_attrs(self):
+        def full_name(doc):
+            return "%s %s" % (doc['first_name'], doc['last_name'])
 
-        factory = Factory(self.database, self.collection,
-            first_name='John',
-            last_name='Smith',
-            age=age_function)
+        factory = Factory(self.collection,
+            first_name = 'John',
+            last_name = 'Smith',
+            full_name = full_name,
+            age = 32)
 
         self.assertDictEqual(factory.build(), {
             "first_name": "John",
             "last_name": "Smith",
+            "full_name": "John Smith",
             "age": 32
         })
+
+    def test_build_with_overrides(self):
+        factory = Factory(self.collection,
+            first_name='John',
+            last_name='Smith',
+            age=32)
+
+        self.assertDictEqual(factory.build(first_name='Mike', age=45), {
+            "first_name": "Mike",
+            "last_name": "Smith",
+            "age": 45
+        })
+
+    def test_build_with_function_and_overrides(self):
+        def full_name(doc):
+            return "%s %s" % (doc['first_name'], doc['last_name'])
+
+        factory = Factory(self.collection,
+            first_name = 'John',
+            last_name = 'Smith',
+            full_name = full_name,
+            age = 32)
+
+        self.assertDictEqual(factory.build(first_name='Mike', age=45), {
+            "first_name": "Mike",
+            "last_name": "Smith",
+            "full_name": "Mike Smith",
+            "age": 45
+        })
+
+    def test_create_simple(self):
+        to_return = {
+            "_id": ObjectId(),
+            "first_name": "John",
+            "last_name": "Smith",
+            "age": 32
+        }
+
+        self.collection.insert = Mock(return_value=to_return["_id"])
+        self.collection.find_one = Mock(return_value=to_return)
+
+        factory = Factory(self.collection,
+            first_name='John',
+            last_name='Smith',
+            age=32)
+
+        created = factory.create()
+
+        self.collection.insert.assert_called_with({
+            "first_name": "John",
+            "last_name": "Smith",
+            "age": 32
+        })
+        self.collection.find_one.assert_called_with(to_return["_id"])
+        self.assertDictEqual(created, to_return)
+
+    def test_create_with_overrides(self):
+        to_return = {
+            "_id": ObjectId(),
+            "first_name": "Mike",
+            "last_name": "Smith",
+            "age": 32
+        }
+
+        self.collection.insert = Mock(return_value=to_return["_id"])
+        self.collection.find_one = Mock(return_value=to_return)
+
+        factory = Factory(self.collection,
+            first_name='John',
+            last_name='Smith',
+            age=32)
+
+        created = factory.create(first_name='Mike')
+
+        self.collection.insert.assert_called_with({
+            "first_name": "Mike",
+            "last_name": "Smith",
+            "age": 32
+        })
+        self.collection.find_one.assert_called_with(to_return["_id"])
+        self.assertDictEqual(created, to_return)
