8e77313ddd6673998e0b6ad3adee5735aec88745
Tom Leach <tom@gc.io>
2013-02-27 18:22:54 -0500

    Added project setup files.

---
+++ setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="Monufacture",
+    version="0.1.0",
+    author="Tom Leach",
+    author_email="tom@leach.it",
+    description="A lightweight factory framework for creating / tearing " +
+                "down test data in MongoDB",
+    license="BSD",
+    keywords="mongo mongodb database testing factory pymongo",
+    url="http://github.com/tleach/monufacture",
+    packages=["monufacture"],
+    long_description="Monufacture is a factory framework for creating test " +
+                     "data in MongoDB, inspired by factory_girl.",
+    install_requires=['pymongo'],
+    tests_require=['mock', 'nose']
+    )
