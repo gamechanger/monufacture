86fe372443b7aa5681c736a42367b99c3c4b3daa
Tom Leach <tom@gc.io>
2013-03-12 09:15:54 -0400

    Incremented version to 0.2.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="Monufacture",
-    version="0.2.1",
+    version="0.2.2",
     author="Tom Leach",
     author_email="tom@leach.it",
     description="A lightweight factory framework for creating / tearing " +
