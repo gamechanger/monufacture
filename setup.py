f53c62384e97e3b6a35eef2f3022ea07444a0c24
Tom Leach <tom@gc.io>
2013-04-04 09:44:40 -0400

    Updated setup.py to reflect new repo location.

--- setup.py
+++ setup.py
@@ -4,15 +4,15 @@ setuptools.setup(
     name="Monufacture",
     version="0.2.7",
     author="Tom Leach",
-    author_email="tom@leach.it",
-    description="A lightweight factory framework for creating / tearing " +
-                "down test data in MongoDB",
+    author_email="tom@gc.io",
+    description="A lightweight factory framework for easily generating test data in MongoDB",
     license="BSD",
     keywords="mongo mongodb database testing factory pymongo",
-    url="http://github.com/tleach/monufacture",
+    url="http://github.com/gamechanger/monufacture",
     packages=["monufacture"],
-    long_description="Monufacture is a factory framework for creating test " +
-                     "data in MongoDB, inspired by factory_girl.",
+    long_description="Monufacture is a factory framework with an API designed to make " +
+                     "it as easy as possible to generate valid test data in MongoDB. " +
+                     "Inspired by the excellent factory_girl Ruby Gem.",
     install_requires=['pymongo'],
     tests_require=['mock', 'nose', 'freezegun']
 )
