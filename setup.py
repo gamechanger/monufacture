eabdbc1d6235d2783e5c73a9e4f015c2a5ca97f8
Jordan Singleton <jordansingleton@Jordans-MacBook-Pro.local>
2015-03-04 15:41:24 -0500

    version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="Monufacture",
-    version="0.3.3",
+    version="1.0.0",
     author="Tom Leach",
     author_email="tom@gc.io",
     description="A lightweight factory framework for easily generating test data in MongoDB",
